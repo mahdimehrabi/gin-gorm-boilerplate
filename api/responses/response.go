f3eaf6d72a3696775010eedab67ad30369d7bee6
mahdimehrabi <mahdimehrabi17@gmail.com>
2022-03-01 12:12:03 +0330

    Make message to msg in responses

--- api/responses/response.go
+++ api/responses/response.go
@@ -6,22 +6,22 @@ import (
 
 // JSON : json response function
 func JSON(c *gin.Context, statusCode int, data interface{}, message string) {
-	c.JSON(statusCode, gin.H{"data": data, "message": message, "ok": true})
+	c.JSON(statusCode, gin.H{"data": data, "msg": message, "ok": true})
 }
 
 // ErrorJSON : json error response function
 func ErrorJSON(c *gin.Context, statusCode int, data interface{}, message string) {
-	c.JSON(statusCode, gin.H{"data": data, "message": message, "ok": false})
+	c.JSON(statusCode, gin.H{"data": data, "msg": message, "ok": false})
 }
 
 func ValidationErrorsJSON(c *gin.Context, statusCode int, data interface{}, message string) {
 	if message == "" {
 		message = "Please fill all required field"
 	}
-	c.JSON(statusCode, gin.H{"data": data, "message": message, "ok": false})
+	c.JSON(statusCode, gin.H{"data": data, "msg": message, "ok": false})
 }
 
 // JSONCount : json response function
 func JSONCount(c *gin.Context, statusCode int, data interface{}, message string, count int64) {
-	c.JSON(statusCode, gin.H{"data": data, "count": count, "message": message, "ok": true})
+	c.JSON(statusCode, gin.H{"data": data, "count": count, "msg": message, "ok": true})
 }
