c7fcfa2cd5ff94dc13d086bad6e7e7c2e7210b03
mahdimehrabi <mahdimehrabi17@gmail.com>
2022-03-03 19:32:38 +0330

    Completed TestPing

--- tests/generic_test.go
+++ tests/generic_test.go
@@ -1,16 +1,17 @@
 package tests
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 )
 
 func (suite *TestSuiteEnv) TestPing() {
 	router := suite.router.Gin
+	a := suite.Assert()
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/ping", nil)
-	fmt.Println(router)
 	router.ServeHTTP(w, req)
+	a.Equal(w.Code, http.StatusOK, "Status code problem")
+	a.Contains(w.Body.String(), "pong", "Contain data problem")
 }
