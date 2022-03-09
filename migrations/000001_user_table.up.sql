f426e3435766fccdfc4a56e734402d5991397fc3
mahdimehrabi <mahdimehrabi17@gmail.com>
2022-03-09 15:08:38 +0330

    Fixed syntax error of migration

--- migrations/000001_user_table.up.sql
+++ migrations/000001_user_table.up.sql
@@ -6,7 +6,8 @@ CREATE TABLE "users"
 "email" VARCHAR (50) UNIQUE NOT NULL,
 "password" VARCHAR (300) NOT NULL,
 "full_name" VARCHAR (60) NOT NULL,
-"must_logout" BOOLEAN NOT NULL DEFAULT FALSE 
+"must_logout" BOOLEAN NOT NULL DEFAULT FALSE,
 PRIMARY KEY ("id")
 );
 CREATE INDEX "idx_users_deleted_at" ON "users" ("deleted_at");
+CREATE INDEX "idx_users_created_at" ON "users" ("created_at");
