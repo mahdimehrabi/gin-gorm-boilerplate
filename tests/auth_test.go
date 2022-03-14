75251a246afae1a2e0c6afbad5146dcfa7e99c04
mahdimehrabi <mahdimehrabi17@gmail.com>
2022-03-14 17:42:01 +0330

    Added register test

--- tests/auth_test.go
+++ tests/auth_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"boilerplate/models"
 	"net/http"
 	"net/http/httptest"
 )
@@ -64,4 +65,40 @@ func (suite TestSuiteEnv) TestLogin() {
 	req, _ = http.NewRequest("POST", "/api/auth/login", MapToJsonBytesBuffer(data))
 	router.ServeHTTP(w, req)
 	a.Equal(400, w.Code, "Status code problem")
+}
+
+func (suite TestSuiteEnv) TestRegister() {
+	router := suite.router.Gin
+	db := suite.database.DB
+	a := suite.Assert()
+	var beforeUserCount int64
+	db.Model(models.User{}).Count(&beforeUserCount)
+
+	//test with completed credentials
+	data := map[string]interface{}{
+		"email":    "mahdi@gmail.com",
+		"password": "m12345678",
+		"fullName": "mahdi mehrabi",
+	}
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/api/auth/register", MapToJsonBytesBuffer(data))
+	router.ServeHTTP(w, req)
+	a.Equal(http.StatusOK, w.Code, "Status code problem")
+	response := ExtractResponseAsMap(w)
+	dt, _ := response["data"].(map[string]interface{})
+	accessToken, _ := dt["accessToken"].(string)
+	refreshToken, _ := dt["refreshToken"].(string)
+	a.True(len(accessToken) > 7, "Access token invalid")
+	a.True(len(refreshToken) > 7, "Refresh token invalid")
+	var afterUserCount int64
+	db.Model(models.User{}).Count(&afterUserCount)
+	a.True(afterUserCount == beforeUserCount+1, "User count problem")
+
+	//test with duplicate email
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("POST", "/api/auth/register", MapToJsonBytesBuffer(data))
+	router.ServeHTTP(w, req)
+	a.Equal(http.StatusBadRequest, w.Code, "Status code problem")
+	db.Model(models.User{}).Count(&afterUserCount)
+	a.True(afterUserCount == beforeUserCount+1, "User count problem")
 }
