fc480ce56d668ca3885df2ee314cecf3cdf93817
mahdimehrabi <mahdimehrabi17@gmail.com>
2022-03-23 02:07:29 +0430

    Added random email generator

--- utils/random.go
+++ utils/random.go
@@ -39,4 +39,8 @@ func GenerateRandomDigitSequence(max int) string {
 	return string(b)
 }
 
+func GenerateRandomEmail(length int) string {
+	return "mail" + GenerateRandomCode(length) + "@gmail.com"
+}
+
 var table = [...]byte{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
