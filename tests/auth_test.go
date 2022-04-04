3a70b2a6edadc5a8a8017073c7f17105dd49f892
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-04-04 18:17:37 +0430

    Added test recovery password

--- tests/auth_test.go
+++ tests/auth_test.go
@@ -325,3 +325,52 @@ func (suite TestSuiteEnv) TestForgotEmail() {
 	suite.database.DB.Find(&user)
 	a.Equal(oldToken, user.ForgotPasswordToken)
 }
+
+func (suite TestSuiteEnv) TestRecoveryPassword() {
+	router := suite.router.Gin
+	db := suite.database.DB
+	a := suite.Assert()
+	user := CreateUser("m12345678", db, suite.encryption)
+	suite.database.DB.Model(&user).UpdateColumn("forgot_password_token", utils.GenerateRandomCode(40))
+
+	//test with right info
+	w := httptest.NewRecorder()
+	data := map[string]interface{}{
+		"token":          user.ForgotPasswordToken,
+		"password":       "m987654321",
+		"repeatPassword": "m987654321",
+	}
+
+	req, _, _ := NewAuthenticatedRequest(suite.authService, suite.database, user, "POST", "/api/profile/change-password", utils.MapToJsonBytesBuffer(data))
+
+	router.ServeHTTP(w, req)
+	a.Equal(http.StatusOK, w.Code, "Status code problem")
+	suite.database.DB.Find(&user)
+	encryptedPassword := suite.encryption.SaltAndSha256Encrypt("m987654321")
+	a.Equal(encryptedPassword, user.Password, "encrypt password problem")
+
+	//test with weak password
+	user = CreateUser("m123456789", db, suite.encryption)
+	data = map[string]interface{}{
+		"token":          user.ForgotPasswordToken,
+		"password":       "12345678",
+		"repeatPassword": "12345678",
+	}
+	w = httptest.NewRecorder()
+	req, _, _ = NewAuthenticatedRequest(suite.authService, suite.database, user, "POST", "/api/profile/change-password", utils.MapToJsonBytesBuffer(data))
+	router.ServeHTTP(w, req)
+	a.Equal(http.StatusUnprocessableEntity, w.Code, "Status code problem")
+
+	//test with wrong token
+	user = CreateUser("m123456789", db, suite.encryption)
+	data = map[string]interface{}{
+		"token":          "wrong token",
+		"password":       "m987654321",
+		"repeatPassword": "m987654321",
+	}
+	w = httptest.NewRecorder()
+	req, _, _ = NewAuthenticatedRequest(suite.authService, suite.database, user, "POST", "/api/profile/change-password", utils.MapToJsonBytesBuffer(data))
+	router.ServeHTTP(w, req)
+	a.Equal(http.StatusForbidden, w.Code, "Status code problem")
+
+}
