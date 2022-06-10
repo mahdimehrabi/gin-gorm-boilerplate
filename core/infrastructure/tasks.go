87fedbc273918bf8005b2a36ab0e14b88f263987
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-06-10 14:46:41 +0430

    Added tasks infrastructure file

---
+++ core/infrastructure/tasks.go
@@ -0,0 +1,33 @@
+package infrastructure
+
+import "github.com/hibiken/asynq"
+
+//Tasks -> Tasks Struct
+type Tasks struct {
+	Logger Logger
+	Env    Env
+	Client *asynq.Client
+	Server *asynq.Server
+}
+
+//NewTasks -> return new Tasks struct
+func NewTasks(
+	logger Logger,
+	env Env,
+) Tasks {
+	return Tasks{
+		Logger: logger,
+		Env:    env,
+		Client: asynq.NewClient(asynq.RedisClientOpt{Addr: env.RedisAddr}),
+		Server: asynq.NewServer(asynq.RedisClientOpt{Addr: env.RedisAddr},
+			asynq.Config{
+				Concurrency: 10,
+				Queues: map[string]int{
+					"critical": 6,
+					"default":  3,
+					"info":     1,
+				},
+			},
+		),
+	}
+}
