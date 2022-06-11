2a3359485514a25c5cbfc3c880ce90138a4e2472
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-06-11 15:48:13 +0430

    Added handleTasks

--- core/tasks/tasks.go
+++ core/tasks/tasks.go
@@ -2,14 +2,36 @@ package tasks
 
 import (
 	"boilerplate/core/infrastructure"
+	"github.com/hibiken/asynq"
 	"go.uber.org/fx"
 )
 
 var TasksModules = fx.Options(
 	fx.Provide(NewTasks),
-	fx.Provide(infrastructure.NewEmail),
 )
 
 type Task interface {
 	HandlesToMux() error
+}
+
+type Tasks struct {
+	logger    infrastructure.Logger
+	taskAsynq TaskAsynq
+}
+
+func NewTasks(logger infrastructure.Logger, taskAsynq TaskAsynq) Tasks {
+	return Tasks{
+		logger:    logger,
+		taskAsynq: taskAsynq,
+	}
+}
+
+func (t *Tasks) HandleTasks() error {
+	emailTask := NewEmailTask(t.logger)
+	serverMux := asynq.NewServeMux()
+	serverMux.HandleFunc(
+		TypeSendVerifyEmail,
+		emailTask.HandleVerifyEmailTask,
+	)
+	return t.taskAsynq.Server.Run(serverMux)
 }
